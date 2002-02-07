267368f6bf15a86fd80a988b955426926783732f
Vince Carey <stvjc@channing.harvard.edu>
2002-02-07 07:03:15 +0000

    ok
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ROC@409 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,35 +1,8 @@
-.First.lib <- function(libname, pkgname, where) {
-    require(methods)
-    require(Biobase)
-    if(missing(where)) {
-        where <- match(paste("package:", pkgname, sep=""), search())
-        if(is.na(where)) {
-            warning(paste("Not a package name: ",pkgname))
-            return()
-        }
-        where <- pos.to.env(where)
-    }
-   .initClasses(where)
-    cacheMetaData(as.environment(where))
-}
-#.First.lib <- function(libname, pkgname, where) {
-#    require(methods)
-#    where <- match(paste("package:", pkgname, sep=""), search())
-#    .initROCclasses(where)
-#    cacheMetaData(as.environment(where))
-#}
-#
-#.First.lib <- function(libname, pkgname, where)
-# { require(methods)
-#  if(missing(where)) {
-#        where <- match(paste("package:", pkgname, sep=""), search())
-#        if(is.na(where)) {
-#            warning(paste("Not a package name: ",pkgname))
-#            return()
-#        }
-#        where <- pos.to.env(where)
-#    }
-#   .initClasses(where)
-#   require(Biobase)
-#}
+.First.lib <- function(libname, pkgname, where)
+ {
+ require(methods)
+ where <- match(paste("package:", pkgname, sep=""), search())
+ .initClasses(where)
+ cacheMetaData(as.environment(where))
+ }
 
