d05577b068fb55ea547fe9cc15a366dacf31761b
jhnzhang <unknown>
2003-11-05 19:41:44 +0000

    Code added for adding vignettes to windows menu.JZ
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ROC@6523 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,10 @@
  require(methods)
  where <- match(paste("package:", pkgname, sep=""), search())
  cacheMetaData(as.environment(where))
+ if(.Platform$OS.type == "windows" && require("Biobase") && interactive()
+        && .Platform$GUI ==  "Rgui"){
+        addPDF2Vig("widgetTools")
+ }
+
  }
 
