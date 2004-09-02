71a6930dffdf413dd15d208dea5db0e314596e7f
unknown <unknown@email>
2004-09-02 18:10:19 +0000

    This commit was manufactured by cvs2svn to create branch 'RELEASE_1_4_branch'.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_1_4_branch/madman/Rpacks/ROC@9859 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+.First.lib <- function(libname, pkgname, where)
+ {
+ require(methods)
+ where <- match(paste("package:", pkgname, sep=""), search())
+ cacheMetaData(as.environment(where))
+
+ if((.Platform$OS.type == "windows") && ("Biobase" %in% installed.packages()[,"Package"])
+    && (interactive()) && (.Platform$GUI ==  "Rgui")){
+     if (require("Biobase"))
+         addVigs2WinMenu("ROC")
+ }
+
+}
+
