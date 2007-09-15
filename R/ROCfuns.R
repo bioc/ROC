e85213ee20eba8ebb33445d57fd496b9031d5c61
Vince Carey <stvjc@channing.harvard.edu>
2007-09-15 11:40:28 +0000

    rationalized graphics -- plot, lines, points, ... handled better
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ROC@27174 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ROCfuns.R
+++ R/ROCfuns.R
@@ -4,69 +4,115 @@
         caseLabel = "character"))
     setGeneric("plot")
 
-    setMethod("plot", c("rocc", "missing"), function (x, y, ...)
-    {
-        nna <- function(x) !is.na(x)
-        object <- x
-        dots <- list(...)
-        dNames <- names(dots)
-        show.thresh <- FALSE
-        add <- FALSE
-        jit <- FALSE
-        line <- FALSE
-        if (nna(ind <- match("show.thresh", dNames))) {
-            show.thresh <- dots$show.thresh
-            dots <- dots[-ind]
-            dNames <- dNames[-ind]
-        }
-        if (nna(ind <- match("add", dNames))) {
-            add <- dots$add
-            dots <- dots[-ind]
-            dNames <- dNames[-ind]
-        }
-        if (nna(ind <- match("jit", dNames))) {
-            jit <- dots$jit
-            dots <- dots[-ind]
-            dNames <- dNames[-ind]
-        }
-        if (nna(ind <- match("line", dNames))) {
-            line <- dots$line
-            dots <- dots[-ind]
-            dNames <- dNames[-ind]
-        }
-        x <- 1 - object@spec
-        y <- object@sens
-        if (!line) {
-            if (add) {
-                if (!jit)
-                    points(x, y, ...)
-                else points(jitter(x), jitter(y), ...)
-            }
-            else {
-                knownArgs <- list(x = x, y = y, xlab = paste("1-spec:",
-                    object@markerLabel), ylab = paste("sens:", object@caseLabel))
-                do.call("plot.default", unlist(list(knownArgs, dots),
-                    recursive = FALSE))
-            }
-        }
-        else {
-            if (add) {
-                if (!jit)
-                    lines(x, y, ...)
-                else lines(jitter(x), jitter(y), ...)
-            }
-            else {
-                knownArgs <- list(x = x, y = y, xlab = paste("1-spec:",
-                    object@markerLabel), ylab = paste("sens:", object@caseLabel),
-                    type = "l")
-                do.call("plot.default", unlist(list(knownArgs, dots),
-                    recursive = FALSE))
-            }
-        }
-        if (show.thresh)
-            text(x, y - 0.04, as.character(round(object@cuts, 3)),
-                cex = 0.7)
-    })
+# sept 2007 -- options to be supported are add, jit, line, show.thresh
+# they should be defaulted and all else goes legitimately to plot.default
+
+setMethod("plot", "rocc", function(x, y, ..., add=FALSE, jit=FALSE, 
+  line=TRUE, show.thresh=FALSE, threshCex=.7, threshYsh=-0.04,
+  threshDig=2 ) {
+  rocObj = x
+  x = 1 - rocObj@spec
+  y = rocObj@sens
+  if (jit) {
+	x = jitter(x)
+	y = jitter(y)
+	}
+  if (add) {
+    if (line) lines(x, y, ...)
+    else points(x, y, ...)
+    }
+  else if (!add) {
+    if (line) plot(x, y, type="l", ...)
+    else plot(x, y, ...)
+  }
+  else stop("supplied add parameter not logical")
+  if (show.thresh) text(x, y + threshYsh, 
+                as.character(round(rocObj@cuts, threshDig)),
+                cex =threshCex)
+  invisible(list(x.used=x, y.used=y))
+})
+
+setGeneric("lines", useAsDefault=lines)
+setMethod("lines", "rocc", function(x, y, ..., jit=FALSE,
+  show.thresh = FALSE, threshCex=.7, threshYsh=-0.04,
+  threshDig=2 ) {
+   plot(x, add=TRUE, line=TRUE, jit=jit, show.thresh=show.thresh,
+      threshCex=threshCex, threshYsh=threshYsh, threshDig=threshDig,
+      ...) })
+
+setGeneric("points", useAsDefault=points)
+setMethod("points", "rocc", function(x, y, ..., jit=FALSE,
+  show.thresh = FALSE, threshCex=.7, threshYsh=-0.04,
+  threshDig=2 ) {
+   plot(x, add=TRUE, line=FALSE, jit=jit, show.thresh=show.thresh,
+      threshCex=threshCex, threshYsh=threshYsh, threshDig=threshDig,
+      ...) })
+  
+  
+
+##    setMethod("plot", c("rocc", "missing"), function (x, y, ...)
+#    {
+#        nna <- function(x) !is.na(x)
+#        object <- x
+#        dots <- list(...)
+#        dNames <- names(dots)
+#        show.thresh <- FALSE
+#        add <- FALSE
+#        jit <- FALSE
+#        line <- FALSE
+#        if (nna(ind <- match("show.thresh", dNames))) {
+#            show.thresh <- dots$show.thresh
+#            dots <- dots[-ind]
+#            dNames <- dNames[-ind]
+#        }
+#        if (nna(ind <- match("add", dNames))) {
+#            add <- dots$add
+#            dots <- dots[-ind]
+#            dNames <- dNames[-ind]
+#        }
+#        if (nna(ind <- match("jit", dNames))) {
+#            jit <- dots$jit
+#            dots <- dots[-ind]
+#            dNames <- dNames[-ind]
+#        }
+#        if (nna(ind <- match("line", dNames))) {
+#            line <- dots$line
+#            dots <- dots[-ind]
+#            dNames <- dNames[-ind]
+#        }
+#        x <- 1 - object@spec
+#        y <- object@sens
+#        if (!line) {
+#            if (add) {
+#                if (!jit)
+#                    points(x, y, ...)
+#                else points(jitter(x), jitter(y), ...)
+#            }
+#            else {
+#                knownArgs <- list(x = x, y = y, xlab = paste("1-spec:",
+#                    object@markerLabel), ylab = paste("sens:", object@caseLabel))
+#                do.call("plot.default", unlist(list(knownArgs, dots),
+#                    recursive = FALSE))
+#            }
+#        }
+#        else {
+#            if (add) {
+#                if (!jit)
+#                    lines(x, y, ...)
+#                else lines(jitter(x), jitter(y), ...)
+#            }
+#            else {
+#                knownArgs <- list(x = x, y = y, xlab = paste("1-spec:",
+#                    object@markerLabel), ylab = paste("sens:", object@caseLabel),
+#                    type = "l")
+#                do.call("plot.default", unlist(list(knownArgs, dots),
+#                    recursive = FALSE))
+#            }
+#        }
+#        if (show.thresh)
+#            text(x, y - 0.04, as.character(round(object@cuts, 3)),
+#                cex = 0.7)
+#    })
 
 "AUC" <-
 function (rocobj)
